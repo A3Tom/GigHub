6c6bd20b7848c6e3adc05ac4b90eaef47435ef3e
Tom Smith <Tom_Esq@Yahoo.co.uk>
2017-07-24 22:46:09 +0100

    Restarted Project with correct auth settings

--- GigHub/GigHub/Classes/MyDbContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Data.Entity;
-
-namespace GigHub.Classes
-{
-    public class MyDbContext : DbContext
-    {
-    }
-}
--- GigHub/GigHub/Properties/AssemblyInfo.cs
+++ GigHub/GigHub/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5cbb929e-357c-4bb4-b833-6a11f884bc3e")]
+[assembly: Guid("82103b97-fa49-43cd-857a-05b3ffa967b3")]
 
 // Version information for an assembly consists of the following four values:
 //
