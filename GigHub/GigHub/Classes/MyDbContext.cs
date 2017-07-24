eb505087ac76772bac8f970d532490c1bb94d3fd
Tom Smith <Tom_Esq@Yahoo.co.uk>
2017-07-24 22:35:19 +0100

    Enabled code first migrations

---
+++ GigHub/GigHub/Classes/MyDbContext.cs
@@ -0,0 +1,8 @@
+﻿using System.Data.Entity;
+
+namespace GigHub.Classes
+{
+    public class MyDbContext : DbContext
+    {
+    }
+}
---
+++ GigHub/GigHub/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace GigHub.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<GigHub.Classes.MyDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(GigHub.Classes.MyDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
